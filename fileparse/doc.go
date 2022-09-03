5630b910185922100a188d0523c83b177831a52e
Nick Wells <wells.nicholas@gmail.com>
2022-09-03 10:35:04 +0100

    refactor: minor changes to keep gofumpt quiet

--- fileparse/doc.go
+++ fileparse/doc.go
@@ -1,5 +1,4 @@
 /*
-
 Package fileparse provides a standard mechanism for parsing a file. It
 supports comments and include directives.
 
@@ -7,6 +6,5 @@ You first construct a file parser and set any options and a line parser
 specific to the type of file you are working with and then call Parse to
 process the file. Any errors found while parsing will be returned for further
 processing.
-
 */
 package fileparse
